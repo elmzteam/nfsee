8266b840fb4425e652613e367acea0d4571d7bed
Zachary Wade <zacharywade@gmail.com>
2018-03-27 14:13:34 -0400

    almost done

---
+++ client/js/index.js
@@ -0,0 +1,41 @@
+let ractive = Ractive({
+	target: "#output",
+	template: "#main",
+	data: {
+		records: [],
+		smoke: 0,
+	}
+})
+
+let clear_log = x => {
+	console.log(x);
+	return x;
+}
+
+ractive.on({
+	"addElement" : () => 
+		ractive.set("showHelp", true)
+			.then(() => ractive.animate("smoke", 1, {duration: 200}))
+			.then(() => fetch("/add"))
+			.then(() => ractive.animate("smoke", 0, {duration: 200}))
+			.then(() => ractive.set("showHelp", false))
+			.then(update),
+	"emulateMe": (id) =>
+		ractive.set("showEmu", true)
+			.then(() => ractive.animate("smoke", 1, {duration: 200}))
+			.then(() => fetch(`/emu/${id}`)),
+	"cancelEmu": () =>
+		fetch("/cancel")
+			.then(() => ractive.animate("smoke", 0, {duration: 200}))
+			.then(() => ractive.set("showEmu", false))
+	
+})
+
+let update = () =>
+	fetch("/data")
+		.then(body => body.json())
+		.then(clear_log)
+		.then(objs => ractive.set({records: objs}))
+
+update()
+
--- main.py
+++ main.py
@@ -1,32 +1,89 @@
-#import nxppy
 import time
-
+import json
+from subprocess import Popen
 from threading import Thread
-
-from flask import Flask
+from flask import Flask, request, send_from_directory
+from os import path
 
 app = Flask(__name__)
 
-records = {}
+records = [ 
+    {"id": 1, "url": "/assets/img_1.jpg"}        
+]
+
+data = {
+    "1": "Zach's Card"     
+}
+
+currentProc = None
 
 @app.route('/')
 def start():
-	return "test"
+    return send_from_directory("client", "index.html")
 
-def monitor_records():
-	global records
-	mifare = nxppy.Mifare()
-	print("Starting")
-	while True:
-		try:
-			val = mifare.select()
-			for i in range(64):
-				print mifare.read_block(i)
-		except Exception:
-			pass
-		time.sleep(1)
+@app.route('/js/<file_name>')
+def send_js (file_name):
+    return send_from_directory("client/js", file_name)
 
-#monitor_records()
-monitor = Thread(target = monitor_records)
-#monitor.start()
-app.run('0.0.0.0', 1337)
+
+@app.route('/css/<file_name>')
+def send_css (file_name):
+    return send_from_directory("client/css", file_name)
+
+
+@app.route('/assets/<file_name>')
+def send_asset (file_name):
+    return send_from_directory("assets", file_name)
+
+
+@app.route('/data')
+def send_data():
+    return json.dumps(records)
+
+@app.route('/add')
+def add_figure():
+    import nxppy
+    import picamera
+    camera = picamera.PiCamera()
+    global records, data
+    mifare = nxppy.Mifare()
+    print("Starting")
+    while True:
+        try:
+            val = mifare.select()
+            value = ""
+            for i in range(64):
+                value += mifare.read_block(i)
+            time.sleep(1)
+            name = path.join("assets", "img_{}.jpg".format(len(records)))
+            picamera.capture(name)
+            data[str(len(records))] = value 
+            records.push({"id": len(records), url: name})
+        except Exception as e:
+            print (e)
+            pass
+        time.sleep(1)
+    time.sleep(4)
+    return ""
+
+@app.route('/emu/<id_name>')
+def start_emulation(id_name):
+    global currentProc
+    if currentProc is not None:
+        currentProc.kill()
+        currentProc = None
+    try:
+            currentProc = Popen(["explorenfc-cardemulation", "-t", data[id_name]]) 
+    except Exception:
+        pass
+    return ""
+
+@app.route('/cancel')
+def stop_emulation():
+    global currentProc
+    if currentProc is not None:
+        currentProc.kill()
+    return ""
+
+if __name__ == "__main__":
+    app.run("0.0.0.0", 1337)
